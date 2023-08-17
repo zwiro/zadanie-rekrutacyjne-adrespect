66a384a2482d0dc55fb67286b0f981056f759362
zwiro <josiu100@gmail.com>
2023-08-17 15:18:52 +0200

    Added masonry library and styled displayed images

---
+++ src/js/masonry.js
@@ -0,0 +1,6 @@
+const elem = document.querySelector(".masonry-grid");
+const gridItem = document.querySelector(".masonry-grid-item");
+const msnry = new Masonry(elem, {
+  itemSelector: ".masonry-grid-item",
+  gutter: Number(getComputedStyle(gridItem).getPropertyValue("--gutter")),
+});
