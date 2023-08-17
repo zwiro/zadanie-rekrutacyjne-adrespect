4af9f8ac531b4b9eca4846b1c0c2619f02572650
zwiro <josiu100@gmail.com>
2023-08-17 18:53:06 +0200

    Fixed expanding images section

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,13 @@ expandImagesBtn.addEventListener("click", () => {
   if (imagesContainer.classList.contains("max-h-[1000vh]")) {
     expandImagesBtn.innerHTML =
       'Rozwiń <img src="./assets/arrow-down-black.svg" alt="" />';
+    imagesContainer.classList.add("max-h-[200vh]");
   } else {
     expandImagesBtn.innerHTML =
       'Zwiń <img src="./assets/arrow-down-black.svg" alt="" class="rotate-180" />';
+    imagesContainer.classList.remove("max-h-[200vh]");
   }
+  expandImagesBtn.classList.toggle("bg-gray/50");
   imagesOverlay.classList.toggle("opacity-0");
   imagesContainer.classList.toggle("max-h-[1000vh]");
 });
