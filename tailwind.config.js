e10136ae3c1c50e40a80079a300aa9a26eae546d
zwiro <josiu100@gmail.com>
2023-08-21 15:25:41 +0200

    Added scroll fade in animation with intersection observer

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,28 @@ module.exports = {
         green: "#1B5B31",
         black: "#111111",
       },
+      letterSpacing: {
+        1: "-0.00875rem",
+        2: "-0.0075rem",
+        3: "-0.01rem",
+        4: "-0.02rem",
+        5: "-0.0875rem",
+        6: "-0.09rem",
+      },
+      lineHeight: {
+        115: "115%",
+        120: "120%",
+        150: "150%",
+        hero: "4.375rem",
+      },
+      keyframes: {
+        fadeIn: {
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 1s",
+      },
     },
   },
   plugins: [],
