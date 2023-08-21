97ee924337e3130f8c697e21c6152f447ede2926
zwiro <josiu100@gmail.com>
2023-08-21 22:26:48 +0200

    Final commit

--- docs/assets/masonry.js
+++
@@ -1,8 +0,0 @@
-const elem = document.querySelector(".masonry-grid"),
-  gridItem = document.querySelector(".masonry-grid-item");
-imagesLoaded(elem, () => {
-  new Masonry(elem, {
-    itemSelector: ".masonry-grid-item",
-    gutter: Number(getComputedStyle(gridItem).getPropertyValue("--gutter")),
-  });
-});
